996ae41b88bf7800fcc09f1dbbe72182cd1c5de8
ChristelleAziz <christelleaziz24@gmail.com>
2024-05-19 18:10:26 -0400

    creation of Util class

---
+++ src/main/java/util/Util.java
@@ -0,0 +1,14 @@
+package util;
+
+public class Util {
+    /**
+     * converts each word in a string to title case
+     * @param strIn the input string
+     * @return the string in title case
+     */
+    public static String toTiTleCase(String strIn) {
+        //TODO : to be implemented
+        String strOut = "";
+        return strOut;
+    }
+}
