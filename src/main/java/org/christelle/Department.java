3418b09411095ff7ea9eb5e2f3535a4300342055
ChristelleAziz <christelleaziz24@gmail.com>
2024-05-19 17:32:35 -0400

    Fields + Methods for the Department class

--- src/main/java/org/christelle/Department.java
+++ src/main/java/org/christelle/Department.java
@@ -1,4 +1,33 @@
 package org.christelle;
 
+import lombok.*;
+
+@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Department {
+    private String departmentId;
+    private String departmentName;
+    private static int nextId;
+
+    /**
+     * takes a department name and checks if it is valid or not
+     * @param departmentName the input department name
+     * @return if it is valid or nod
+     */
+    public static boolean validateDepartmentName(String departmentName) {
+        //TODO : to be implemented
+        return false;
+    }
+
+    public Department(String departmentId, String departmentName) {
+        if (validateDepartmentName(departmentName)) {
+            this.departmentId = departmentId;
+            this.departmentName = departmentName;
+        } else {
+            this.departmentId = null;
+            this.departmentName = null;
+        }
+    }
 }
