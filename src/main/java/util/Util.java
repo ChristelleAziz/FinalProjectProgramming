5fecd14cfca4fa1314f4a8e5e9834532fd7ccbf7
ChristelleAziz <christelleaziz24@gmail.com>
2024-05-22 02:46:55 -0400

    implementation of the toTitleCase(String strIn) method in the Util class

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -6,9 +6,18 @@ public class Util {
      * @param strIn the input string
      * @return the string in title case
      */
-    public static String toTiTleCase(String strIn) {
-        //TODO : to be implemented
-        String strOut = "";
-        return strOut;
+    public static String toTitleCase(String strIn) {
+        if (strIn == null || strIn.isEmpty()) {
+            return strIn;
+        }
+
+        String[] words = strIn.split(" ");
+        String firstWord = words[0];
+        String secondWord = words[1];
+
+        String titleCaseFirstWord = firstWord.substring(0, 1).toUpperCase() + firstWord.substring(1).toLowerCase();
+        String titleCaseSecondWord = secondWord.substring(0, 1).toUpperCase() + secondWord.substring(1).toLowerCase();
+
+        return titleCaseFirstWord + " " + titleCaseSecondWord;
     }
 }
