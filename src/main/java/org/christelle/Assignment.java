e58cc55f4134f704f807d24f217fc34f9ff7a025
ChristelleAziz <christelleaziz24@gmail.com>
2024-05-21 23:07:03 -0400

    implementation of all the methods in the Assignment class

--- src/main/java/org/christelle/Assignment.java
+++ src/main/java/org/christelle/Assignment.java
@@ -1,6 +1,7 @@
 package org.christelle;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Assignment {
     private String assignmentId;
@@ -15,14 +16,41 @@ public class Assignment {
      * calculates the average score for one assignment
      */
     public void calcAssignmentAvg() {
-        //TODO: to be implemented
+        if (scores.isEmpty()) {
+            assignmentAverage = 0;
+            return;
+        }
+
+        double sum = 0;
+        for (int score : scores) {
+            sum += score;
+        }
+        assignmentAverage = sum / scores.size();
     }
 
     /***
      * generates random scores for all students in an assignment
      */
     public void generateRandomScore() {
-        //TODO: to be implemented
+        if (scores == null) {
+            scores = new ArrayList<>();
+        }
+
+        Random rand = new Random();
+
+        for (int i = 0; i < scores.size(); i++) {
+            int section = rand.nextInt(0, 11);
+            int score = switch (section) {
+                case 0 -> rand.nextInt(1, 10);
+                case 1, 2 -> rand.nextInt(60, 70);
+                case 3, 4 -> rand.nextInt(70, 80);
+                case 5, 6, 7, 8 -> rand.nextInt(80, 90);
+                case 9, 10 -> rand.nextInt(90, 101);
+                default -> -1;
+            };
+
+            scores.add(score);
+        }
     }
 
     @Override
